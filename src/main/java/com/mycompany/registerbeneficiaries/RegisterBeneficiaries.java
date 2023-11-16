451bdb3e8a7d5b25dfc8b636a7ceb47589dc40ee
Juan David Ortiz <judavid_97@hotmail.com>
2023-11-16 00:41:36 -0500

    0001: Primera versión del programa, se crean modulos y validaciones para subir archivo y registrar usuarios en el

---
+++ src/main/java/com/mycompany/registerbeneficiaries/RegisterBeneficiaries.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registerbeneficiaries;
+
+/**
+ *
+ * @author judav
+ */
+public class RegisterBeneficiaries {
+
+}
