99cf0e41e0aa0fa664cbd5988589374bd8f29145
Juan David Ortiz <judavid_97@hotmail.com>
2023-11-27 10:42:05 -0500

    0004: Seacrh and modify beneficiaries

--- src/main/java/interfaces/DAOBeneficiaries.java
+++ src/main/java/interfaces/DAOBeneficiaries.java
@@ -6,7 +6,9 @@ import models.Beneficiaries;
 
 public interface DAOBeneficiaries {
     public boolean insert(Beneficiaries beneficiarie) throws Exception;
-    public void modify(Beneficiaries beneficiarie) throws Exception;
+    public boolean modify(Beneficiaries beneficiarie) throws Exception;
     public void delete(String idBeneficiarie) throws Exception;
     public List<Beneficiaries> list() throws Exception;
+    public List<Beneficiaries> listByName(String names) throws Exception;
+    public Beneficiaries getBeneficiarieById(int idBeneficiarie) throws Exception;
 }
